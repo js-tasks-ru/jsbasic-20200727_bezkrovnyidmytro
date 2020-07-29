288d3352bb047908005dc5d6d57b41282f996ed4
bezkrovnyidmytro <dmytro.bezkrovny@catenamedia.com>
2020-07-29 17:34:50 +0300

    Add solution for 1 module - 4 task (checkSpam function)

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -5,4 +5,10 @@
  */
 function checkSpam(str) {
   // ваш код...
+    str = String(str).toLowerCase();
+
+    if(str.includes('xxx') || str.includes('1xbet')) {
+        return true;
+    }
+    return false;
 }
