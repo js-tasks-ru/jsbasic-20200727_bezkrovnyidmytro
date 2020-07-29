f8f40cc8e93c080d391265fcfe97706f37aab825
bezkrovnyidmytro <dmytro.bezkrovny@catenamedia.com>
2020-07-29 17:46:18 +0300

    Add solution for 1 module - 5 task (truncate function)

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -6,4 +6,16 @@
  */
 function truncate(str, maxlength) {
   // ваш код...
+    str = String(str);
+
+    if(str.length < maxlength) {
+        return str;
+    }
+
+    const except_symbols = '…';
+    const except_symbols_length = except_symbols.length;
+
+    str = str.slice(0, maxlength - except_symbols_length);
+
+    return str + except_symbols;
 }
