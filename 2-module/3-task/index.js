c07b097c6b2d69e172c369bbbe9e48e2cac0750c
bezkrovnyidmytro <dmytro.bezkrovny@catenamedia.com>
2020-08-03 22:56:04 +0300

    Add solution for 2 module - 3 task (calculator object)

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,21 @@
 let calculator = {
-  // ваш код
+  a: 0,
+  b: 0,
+  read: function() {
+    let a = prompt('Please enter first number (integer): ');
+    let b = prompt('Please enter second number (integer): ');
+
+    this.a = Number(a);
+    this.b = Number(b);
+
+    return true;
+  },
+  sum: function() {
+    return this.a + this.b;
+  },
+  mul: function() {
+    return this.a * this.b;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
