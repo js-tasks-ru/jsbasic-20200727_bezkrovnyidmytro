5c0d7de9049fc1e7e03ac284f1b942822c6e6789
bezkrovnyidmytro <dmytro.bezkrovny@catenamedia.com>
2020-08-09 21:07:29 +0300

    Add solution for 3 module - 3 task (camelize function)

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,6 +2,36 @@
  * @param {string} str
  * @returns {string}
  */
+function ucFirst(str) {
+  str = String(str)
+
+  let first_letter = str.slice(0, 1);
+  let first_letter_upper_case = first_letter.toUpperCase();
+  let remain_str = str.slice(1);
+
+  return first_letter_upper_case + remain_str;
+}
+
+/**
+ * @param {string} str
+ * @returns {string}
+ */
 function camelize(str) {
-  // ваш код...
+  const sep = '-';
+
+  if (!(str.includes(sep))) {
+    return str;
+  }
+
+  let strSplit = str.split(sep);
+
+  if (!strSplit) {
+    return str;
+  }
+
+  return strSplit
+    .map((item, index) => {
+      return index ? ucFirst(item) : item;
+    })
+    .join('');
 }
