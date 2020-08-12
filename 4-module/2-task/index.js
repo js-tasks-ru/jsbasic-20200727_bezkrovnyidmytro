790704686b0ff13364feb5496a7517e8088622d2
bezkrovnyidmytro <dmytro.bezkrovny@catenamedia.com>
2020-08-12 21:07:32 +0300

    Add solution for 4 module - 2 task (makeDiagonalRed function)

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,11 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+  for (let row of table.rows) {
+    for (let col of row.cells) {
+      if (row.rowIndex === col.cellIndex) {
+        col.style.backgroundColor = 'red';
+      }
+    }
+  }
 }
