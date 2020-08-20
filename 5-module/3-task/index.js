87e21c9eb4e91bda5ece4f0680f04afb44a30092
bezkrovnyidmytro <dmytro.bezkrovny@catenamedia.com>
2020-08-20 22:24:05 +0300

    Add solution for 5 module - 3 task (initCarousel function)

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,51 @@
 function initCarousel() {
-  // ваш код...
+  let nextBtn = document.querySelector('.carousel__arrow_right');
+  let prevBtn = document.querySelector('.carousel__arrow_left');
+
+  const numberSlides = document.querySelectorAll('.carousel__slide').length;
+  let currentSliderPosition = 1;
+
+  let sum = (a, b) => a + b;
+  let diff = (a, b) => a - b;
+
+  let getElementOffsetWidth = (element) => {
+    let OffsetWidth = Number(element.offsetWidth);
+    let PrevOffsetWidth = Number(element.style.transform.replace(/[^-?\d+]/g, ''));
+
+    return {OffsetWidth, PrevOffsetWidth};
+  };
+
+  let slideOnClick = (op) => {
+    let carouselInner = document.querySelector('.carousel__inner');
+    let offsets = getElementOffsetWidth(carouselInner);
+
+    let transformValue = op(offsets.PrevOffsetWidth, offsets.OffsetWidth);
+    carouselInner.style.transform = `translateX(${transformValue}px)`;  
+  };
+
+  let hideArrows = (index) => {
+    if (index === numberSlides) {
+      nextBtn.style.display = 'none';
+    } else {
+      nextBtn.style.display = '';
+    }
+
+    if (index === 1) {
+      prevBtn.style.display = 'none';
+    } else {
+      prevBtn.style.display = '';
+    }
+  };
+
+  hideArrows(currentSliderPosition);
+
+  nextBtn.addEventListener('click', (eventClick) => { 
+    slideOnClick(diff);
+    hideArrows(++currentSliderPosition);
+  });
+
+  prevBtn.addEventListener('click', (eventClick) => {
+    slideOnClick(sum);
+    hideArrows(--currentSliderPosition);
+  });
 }
